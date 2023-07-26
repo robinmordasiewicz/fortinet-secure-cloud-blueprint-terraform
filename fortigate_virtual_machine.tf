5ec41757261de0cf39634ea8b94e1a16aa5f3803
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-07-26 17:25:19 -0400

    adding oidc auth script

--- fortigate_virtual_machine.tf
+++ fortigate_virtual_machine.tf
@@ -7,10 +7,10 @@ resource "random_password" "fortigate-admin_password" {
   keepers = {
     resource_group_name = azurerm_resource_group.resource-group.name
   }
-  length = 8
-  min_lower = 1
+  length      = 8
+  min_lower   = 1
   min_special = 1
-  min_upper = 1
+  min_upper   = 1
 }
 resource "azurerm_public_ip" "fortigate-public_ip" {
   name                = "fortigate-public_ip"
@@ -62,8 +62,8 @@ resource "azurerm_network_interface" "fortigate-dmz-network-interface" {
 resource "azurerm_linux_virtual_machine" "fortigate-virtual-machine" {
   name                            = "fortigate"
   computer_name                   = "fortigate"
-  admin_password                  = random_password.fortigate-admin_password
-  admin_username                  = random_pet.fortigate-admin_username
+  admin_username                  = random_pet.fortigate-admin_username.id
+  admin_password                  = random_password.fortigate-admin_password.result
   availability_set_id             = azurerm_availability_set.fortinet-availability-set.id
   disable_password_authentication = false
   location                        = azurerm_resource_group.resource-group.location
@@ -116,8 +116,9 @@ output "fortigate-public_ip_address" {
   value = data.azurerm_public_ip.fortigate-public_ip.ip_address
 }
 output "fortigate-admin_username" {
-  value = random_pet.fortigate-admin_username
+  value = random_pet.fortigate-admin_username.id
 }
 output "fortigate-admin_password" {
-  value = random_pet.fortigate-admin_password
+  value = random_password.fortigate-admin_password.result
+  sensitive = true
 }
--- ubuntu_virtual_machine.tf
+++ ubuntu_virtual_machine.tf
@@ -42,7 +42,7 @@ resource "azurerm_network_interface" "ubuntu-dmz-network-interface" {
 resource "azurerm_linux_virtual_machine" "ubuntu-virtual-machine" {
   name                            = "ubuntu-virtual-machine"
   computer_name                   = "ubuntu"
-  admin_username                  = random_pet.ubuntu-admin_username
+  admin_username                  = random_pet.ubuntu-admin_username.id
   disable_password_authentication = true
   availability_set_id             = azurerm_availability_set.fortinet-availability-set.id
   location                        = azurerm_resource_group.resource-group.location
@@ -50,7 +50,7 @@ resource "azurerm_linux_virtual_machine" "ubuntu-virtual-machine" {
   network_interface_ids           = [azurerm_network_interface.ubuntu-internal-network-interface.id, azurerm_network_interface.ubuntu-dmz-network-interface.id]
   size                            = "Standard_F2s_v2"
   admin_ssh_key {
-    username = random_pet.ubuntu-admin_username
+    username   = random_pet.ubuntu-admin_username.id
     public_key = tls_private_key.ssh-key.public_key_openssh
     #public_key = file("~/.ssh/id_rsa.pub")
   }
@@ -82,5 +82,5 @@ output "ubuntu-public_ip_address" {
 }
 
 output "ubuntu-admin_username" {
-  value = random_pet.ubuntu-admin_username
+  value = random_pet.ubuntu-admin_username.id
 }
