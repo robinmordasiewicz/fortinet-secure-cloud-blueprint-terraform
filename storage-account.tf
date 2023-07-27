1ed67412154f3f98dc40cd7bbf7b4afbcf1c46cf
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-07-27 08:45:33 -0400

    fixing checkov

--- storage-account.tf
+++ storage-account.tf
@@ -17,15 +17,6 @@ resource "azurerm_storage_account" "storage-account" {
   resource_group_name           = azurerm_resource_group.resource-group.name
   public_network_access_enabled = false
   min_tls_version               = "TLS1_2"
-  queue_properties {
-    logging {
-      delete                = true
-      read                  = true
-      write                 = true
-      version               = "1.0"
-      retention_policy_days = 10
-    }
-  }
 }
 
 #resource "azurerm_storage_container" "storage-container" {
