efdfaf6be6c9a185f2f9546af73d03cfdc3a565f
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-07-27 10:20:08 -0400

    fixing checkov

--- subnet.tf
+++ subnet.tf
@@ -1,3 +1,43 @@
+resource "azurerm_network_security_group" "private-nsg" {
+  name                = "private-nsg"
+  location            = azurerm_resource_group.resource-group.location
+  resource_group_name = azurerm_resource_group.resource-group.name
+  security_rule {
+    name                       = "block-all-in"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "block-all-out"
+    priority                   = 101
+    direction                  = "Outbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+resource "azurerm_subnet_network_security_group_association" "external-subnet-association" {
+  subnet_id                 = azurerm_subnet.external-subnet.id
+  network_security_group_id = azurerm_network_security_group.block-all.id
+}
+resource "azurerm_subnet_network_security_group_association" "dmz-subnet-association" {
+  subnet_id                 = azurerm_subnet.dmz-subnet.id
+  network_security_group_id = azurerm_network_security_group.block-all.id
+}
+resource "azurerm_subnet_network_security_group_association" "internal-subnet-association" {
+  subnet_id                 = azurerm_subnet.internal-subnet.id
+  network_security_group_id = azurerm_network_security_group.block-all.id
+}
+
 resource "azurerm_subnet" "external-subnet" {
   address_prefixes     = ["${var.external-Prefix}"]
   name                 = var.external-Name
