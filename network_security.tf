dec33777ea48c12228e3245da110dc84bf7e3608
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-07-27 09:25:38 -0400

    fixing checkov

--- network_security.tf
+++ network_security.tf
@@ -4,13 +4,13 @@ resource "azurerm_network_security_group" "nsg" {
   resource_group_name = azurerm_resource_group.resource-group.name
 
   security_rule {
-    name                       = "allow_all_tcp_sg"
+    name                       = "allow_http-https_tcp"
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "22-443"
+    destination_port_ranges    = ["80","443"]
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
