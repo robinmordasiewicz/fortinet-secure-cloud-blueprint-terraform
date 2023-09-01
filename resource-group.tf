451d2ff5385307fc5037c6cadbf0526eb9802449
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-09-01 17:25:05 -0400

    fixing drift

--- resource-group.tf
+++ resource-group.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "resource-group" {
   location = var.azure_region
   name     = var.resource_group_name
   tags = {
-    Username = var.azure_user
+    Username = var.azure_username
   }
 }
 
