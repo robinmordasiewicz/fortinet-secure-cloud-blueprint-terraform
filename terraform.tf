bc2c327839f1f82075ead30b49d363d3db24e91d
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-09-11 13:10:51 -0400

    getting better

--- terraform.tf
+++ terraform.tf
@@ -5,10 +5,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~>3.71.0"
     }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~>2.41.0"
-    }
+#    azuread = {
+#      source  = "hashicorp/azuread"
+#      version = "~>2.41.0"
+#    }
     random = {
       source  = "hashicorp/random"
       version = "~>3.5.1"
