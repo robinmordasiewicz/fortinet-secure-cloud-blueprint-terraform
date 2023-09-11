3452a2b191fc388f6dc1ff271c9de7733f4fcac2
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-09-11 08:49:04 -0400

    getting better

--- doit.sh
+++ doit.sh
@@ -8,3 +8,19 @@ git checkout Production
 git merge main
 git push origin Production
 git switch main
+
+git checkout main
+git fetch -p origin
+git merge origin/main
+git checkout Development
+git merge main
+git push origin Development
+git switch main
+
+git checkout main
+git fetch -p origin
+git merge origin/main
+git checkout Staging
+git merge main
+git push origin Staging
+git switch main
