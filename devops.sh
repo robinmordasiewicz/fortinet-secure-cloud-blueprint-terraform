e27f1fb3419cf97638503793fd4b3a859cf38777
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-09-17 00:01:30 -0400

    updating fortigate config

--- devops.sh
+++ devops.sh
@@ -3,8 +3,10 @@
 
 #git checkout -b Development
 
+git switch -c Development
+git push --set-upstream origin Development
+git switch main
+git branch -d Development
 git push origin --delete Development
 
-git switch -c Development
 
-git push --set-upstream origin Development
