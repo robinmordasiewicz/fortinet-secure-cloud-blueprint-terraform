e78f8d1df11a22eaf29997af96d8844861a88723
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-10-04 17:02:21 -0400

    installing tflint

--- variables.tf
+++ variables.tf
@@ -2,6 +2,7 @@ variable "AZURE_RESOURCE_GROUP_NAME" {
   description = "Name of the resource group."
   type        = string
 }
+
 variable "vnetAddressPrefix" {
   description = "Virtual Network Address prefix"
   type        = string
