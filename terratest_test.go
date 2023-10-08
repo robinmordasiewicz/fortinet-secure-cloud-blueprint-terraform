ea9c9626312fd95d143bd824eb6295611d515d3e
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-10-07 19:29:19 -1000

    I forget what I added

--- terratest_test.go
+++ terratest_test.go
@@ -10,7 +10,6 @@ import (
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/azure"
-	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
