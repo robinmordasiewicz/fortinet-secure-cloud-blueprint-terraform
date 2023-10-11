2c07521b92172c346f010a6a43d81c5de430efad
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-10-11 13:18:40 -0400

    I forget what I added

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,9 @@
 output "availability_set_name" {
   value = azurerm_availability_set.fortinet-availability-set.name
 }
+output "availability_set_id" {
+  value = azurerm_availability_set.fortinet-availability-set.id
+}
 
 output "availability_set_fdc" {
   value = azurerm_availability_set.fortinet-availability-set.platform_fault_domain_count
