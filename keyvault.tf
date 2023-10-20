d120e9811aa5c7a554ff6afdd1707e9b9331e166
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-10-20 14:25:31 -0400

    trying to get keyvault to work

--- keyvault.tf
+++ keyvault.tf
@@ -132,7 +132,7 @@ variable "key_name" {
 variable "sku_name" {
   type        = string
   description = "The SKU of the vault to be created."
-  default     = "standard"
+  default     = "Premium"
   validation {
     condition     = contains(["standard", "premium"], var.sku_name)
     error_message = "The sku_name must be one of the following: standard, premium."
