c5c573244a73ea65733b06f917239a2bc87e2711
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-10-20 21:08:56 -1000

    trying to get megalinter working

--- resource-group.tf
+++ resource-group.tf
@@ -1,11 +1,3 @@
-#resource "azurerm_resource_group" "resource-group" {
-#  location = var.AZURE_REGION
-#  name     = var.AZURE_RESOURCE_GROUP_NAME
-#  tags = {
-#    Username = var.AZURE_USERNAME
-#  }
-#}
-
 data "azurerm_resource_group" "AZURE_RESOURCE_GROUP" {
   name = var.AZURE_RESOURCE_GROUP_NAME
 }
